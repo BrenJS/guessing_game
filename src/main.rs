287d677f9b54b79752ff74138e037d819ddf6fbf
Brendan Sorohan <brendansorohan@icloud.com>
2022-02-05 20:11:32 +0000

    Trivial: Read standard input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            let trimmed_input = input.trim();
+            input = trimmed_input.to_string();
+        }
+        Err(e) => {println!("{:?}", e);}
+    }
+    println!("Hello, {}!", {input});
 }
