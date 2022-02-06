57c94af22915940166af001f71a1d05e94a2c7b3
Brendan Sorohan <brendansorohan@icloud.com>
2022-02-06 23:26:57 +0000

    trivial: added random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,38 @@
+use rand::Rng;
 use std::io;
+use std::io::Write;
 use std::str::FromStr;
 
 fn main() {
-    
     let max_number = 256;
     println!("Guess a number between 0 and {}", max_number);
+    let mut rng = rand::thread_rng();
+    let random_number = rng.gen_range(0..=max_number);
+    println!("{}", random_number);
+    print!("Your guess: ");
+    io::stdout().flush().unwrap();
     let input: i32 = loop {
         match get_input() {
-            Some(x) => {break x;}
-            None => {}
+            Some(x) => {
+                break x;
+            }
+            None => {
+                println!(
+                    "Not a valid input, try and number between 0 and {}!",
+                    max_number
+                );
+                print!("Your guess: ");
+                io::stdout().flush().unwrap();
+            }
         }
     };
     println!("Hello, {}", &input);
 }
 
-
 fn get_input<T: FromStr>() -> Option<T> {
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
-        Err(_) => {return None},
+        Err(_) => return None,
         _ => {}
     }
     input.trim().parse::<T>().ok()
